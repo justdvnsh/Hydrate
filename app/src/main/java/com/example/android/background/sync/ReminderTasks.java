2cb08b3c26f737a19a5934eac948abe0708360db
justdvnsh <justdvnsh2208@gmail.com>
2019-11-12 14:38:28 +0530

    added intent service to increment water count

--- app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -1,6 +1,23 @@
 package com.example.android.background.sync;
 
+import android.content.Context;
+
+import static com.example.android.background.utilities.PreferenceUtilities.incrementWaterCount;
+
 // TODO (1) Create a class called ReminderTasks
+public class ReminderTasks {
+
+    public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
+
+    public static void executeTask(Context context, String action) {
+
+        if (ACTION_INCREMENT_WATER_COUNT.equals(action)) {
+            incrementWaterCount(context);
+        }
+
+    }
+
+}
 
 // TODO (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
 
